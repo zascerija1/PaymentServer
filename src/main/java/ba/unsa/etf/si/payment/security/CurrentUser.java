288bbb8e62818d8b54ea8523ca2a90dbc52841ae
zascerija1 <zascerija1.etf.unsa.ba>
2020-03-26 16:07:26 +0100

    Initial authentication using jwt together with roles

---
+++ src/main/java/ba/unsa/etf/si/payment/security/CurrentUser.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.si.payment.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
