fea8b71aa50b6b0c22fdc5deb01040201630656c
zascerija1 <zascerija1.etf.unsa.ba>
2020-04-28 18:27:47 +0200

    payment status converter

---
+++ src/main/java/ba/unsa/etf/si/payment/util/PaymentConverter.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.si.payment.util;
+
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class PaymentConverter {
+}
--- src/main/java/ba/unsa/etf/si/payment/util/PaymentStatus.java
+++ src/main/java/ba/unsa/etf/si/payment/util/PaymentStatus.java
@@ -1,10 +1,20 @@
 package ba.unsa.etf.si.payment.util;
 
 public enum PaymentStatus {
-    SUFFICIENT_FUNDS,
-    PROBLEM,
-    CANCELED,
-    PAID,
-    INSUFFICIENT_FUNDS,
-    INVALIDATED
+    SUFFICIENT_FUNDS ("SUFFICIENT_FUNDS"),
+    PROBLEM ("PROBLEM"),
+    CANCELED ("CANCELED"),
+    PAID ("PAID"),
+    INSUFFICIENT_FUNDS ("INSUFFICIENT_FUNDS"),
+    PENDING("PENDING");
+
+    private String status;
+
+    PaymentStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
